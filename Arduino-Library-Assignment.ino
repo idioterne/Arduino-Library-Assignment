9ae892b9904335a7632060a3b034a551e2c96c82
Stephan D <stephan-mdd@gmail.com>
2019-04-30 21:37:57 +0200

    async button added

--- Arduino-Library-Assignment.ino
+++ Arduino-Library-Assignment.ino
@@ -2,8 +2,19 @@
 
 AsyncTrafficLight asyncTrafficLight(10, 9, 8, 7, 6, 5, 1000);
 
+// button configuration
+const int BUTTON_PIN = 2;
+
 void setup(){}
 
 void loop(){
   asyncTrafficLight.start();
+
+  // code here can be exuceted asynchronous while the ligth are waiting... 
+  asyncButton(); 
+}
+
+// sets the LED_BUILTIN to high each time the button is pressed (asynchronous)
+void asyncButton() {
+  digitalWrite(LED_BUILTIN, digitalRead(BUTTON_PIN));
 }
